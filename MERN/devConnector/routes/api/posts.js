241d5e2f510eb1cefd30eb792e8d6662adb2d089
Hussein <hznagri@uwaterloo.ca>
2019-11-06 01:18:44 -0500

    backend routes completed

--- MERN/devConnector/routes/api/posts.js
+++ MERN/devConnector/routes/api/posts.js
@@ -36,7 +36,7 @@ router.post("/", [auth,
     console.error(error.message);
     res.status(500).json({ msg: "Server error" });
   }
-})
+});
 
 //@route  GET api/posts
 //@desc Obtain all of the Posts
@@ -149,5 +149,73 @@ router.put("/unlike/:id", auth, async (req, res) => {
   }
 });
 
+
+//@route  Post api/posts/comment/:id
+//@desc comment on a Post
+//@access private
+router.post("/comment/:id", [auth,
+  [
+    check("text", "Text is required").not().isEmpty(),
+  ]
+], async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    const post = await Post.findById(req.params.id);
+
+
+    const newComment = {
+      text: req.body.text,
+      name: user.name,
+      avatar: user.avatar,
+      user: req.user.id
+    };
+
+    post.comments.unshift(newComment);
+
+
+    await post.save()
+    res.json(post.comments);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ msg: "Server error" });
+  }
+});
+
+//@route  Post api/posts/comment/:id/:comment_id
+//@desc   delete comment
+//@access private
+
+router.delete("/comment/:id/:comment_id", auth, async (req, res) => {
+  try {
+    //get the post
+    const post = await Post.findById(req.params.id);
+    //pull out the comment
+    const comment = post.comments.find(comment => comment.id === req.params.comment_id)
+    //make sure comment exists 
+    if (!comment) {
+      return res.status(404).json({ msg: "Comment was not found" });
+    }
+    //check user
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "User not authorized" });
+    }
+
+
+    //get removeIndex
+    const removeIndex = post.comments.map(comment => comment.user.toString()).indexOf(req.user.id);
+    post.comments.splice(removeIndex, 1);
+    await post.save();
+
+    res.json(post.comments);
+
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ msg: "Server error" });
+  }
+})
 
 module.exports = router
