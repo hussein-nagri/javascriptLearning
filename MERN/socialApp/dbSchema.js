3fa9f542f8bcd0026d5222e95eafabb2804a85d5
Hussein <hznagri@uwaterloo.ca>
2019-12-04 22:37:21 -0500

    added signup route

---
+++ MERN/socialApp/dbSchema.js
@@ -0,0 +1,11 @@
+let db = {
+  screams: [
+    {
+      userHandle: 'user',
+      body: 'this is the scream body',
+      createdAt: '2019-12-05T01:19:44.598Z',
+      likeCount: 5,
+      commentCount: 2
+    }
+  ]
+}
--- MERN/socialApp/functions/index.js
+++ MERN/socialApp/functions/index.js
@@ -1,44 +1,112 @@
 const functions = require('firebase-functions');
 const admin = require("firebase-admin");
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
+const firebase = require('firebase');
+const express = require('express');
+const app = express();
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBffOg0wP8X2oJsSsvs8JnCANtPYOmtBv0",
+  authDomain: "socialapp-8dc24.firebaseapp.com",
+  databaseURL: "https://socialapp-8dc24.firebaseio.com",
+  projectId: "socialapp-8dc24",
+  storageBucket: "socialapp-8dc24.appspot.com",
+  messagingSenderId: "121497261753",
+  appId: "1:121497261753:web:4424eacdd42e2b52136d68",
+  measurementId: "G-THG7EZMP3T"
+};
+
+//for locally
+
 admin.initializeApp({
   credential: admin.credential.cert(require('../key/admin.json'))
 });
 
+// admin.initializeApp();
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello World!");
-});
 
-exports.getScreams = functions.https.onRequest((req, res) => {
-  admin.firestore().collection('screams').get()
+
+firebase.initializeApp(firebaseConfig);
+
+app.get('/screams', (req, res) => {
+  admin
+    .firestore()
+    .collection('screams')
+    .orderBy('createdAt', "desc")
+    .get()
     .then(data => {
       let screams = [];
       data.forEach(doc => {
-        screams.push(doc.data());
+        screams.push({
+          id: doc.id,
+          body: doc.data().body,
+          userHandle: doc.data().userHandle,
+          createdAt: doc.data().createdAt,
+          commentCount: doc.data().commentCount,
+          likeCount: doc.data().likeCount
+        });
       });
       return res.json(screams);
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({ error: err.code });
+    });
 });
 
+//post a scream
+app.post('/scream', (req, res) => {
 
-exports.createScream = functions.https.onRequest((req, res) => {
+  if (req.body.body.trim() === '') {
+    return res.status(400).json({ body: "Body must not be empty" })
+  }
+
   const newScream = {
     body: req.body.body,
     userHandle: req.body.userHandle,
-    createdAt: admin.firestore.Timestamp.fromDate(new Date())
+    createdAt: new Date().toISOString()
+    // likeCount: req.body.likeCount,
+    // commentCount: req.body.commentCount
   }
 
   admin.firestore().collection('screams').add(newScream)
-    // eslint-disable-next-line promise/always-return
     .then(doc => {
-      res.json({ message: `document ${doc.id} created successfully` });
+      return res.json({ message: `document ${doc.id} created successfully` });
     })
     .catch(err => {
-      res.status(500).json({ error: 'something went wrong' }) //server error
       console.log(err)
+      return res.status(500).json({ error: 'something went wrong' }) //server error
     });
 });
+
+//signup route
+app.post("/signup", (req, res) => {
+
+  const newUser = {
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+    handle: req.body.handle
+  };
+
+  //todo: validate data
+
+  firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password)
+    .then(data => {
+      return res.status(201).json({ message: `user${data.user.uid} signed up successfully` })
+    })
+    .catch(err => {
+      console.error(err)
+      return res.status(500).json({ error: err.code })
+    });
+
+
+});
+
+
+
+
+
+
+
+//tell firebase that app will be the new endpoint receiver -> u wanna hit /api https://baseurl.com /api/
+exports.api = functions.https.onRequest(app);
