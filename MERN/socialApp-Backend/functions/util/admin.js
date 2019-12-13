19e974a893d53df04ce8c11e435df80341cfb3eb
Hussein <hznagri@uwaterloo.ca>
2019-12-13 01:45:46 -0500

    change directory

---
+++ MERN/socialApp-Backend/functions/util/admin.js
@@ -0,0 +1,18 @@
+const admin = require("firebase-admin");
+const config = require("../util/config")
+
+
+
+// for locally
+admin.initializeApp({
+  credential: admin.credential.cert(require('../../key/admin.json'))
+});
+
+// admin.initializeApp(config);
+
+
+
+const db = admin.firestore();
+
+
+module.exports = { admin, db };
