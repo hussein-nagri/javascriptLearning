8f665cbfed75c2f0ff51681a959a7b8ac2161470
Hussein <hznagri@uwaterloo.ca>
2019-12-13 00:27:05 -0500

    testing backend route

--- MERN/socialApp/functions/index.js
+++ MERN/socialApp/functions/index.js
@@ -134,13 +134,46 @@ exports.onUserImageChange = functions.firestore.document(`/users/{userId}`)
   .onUpdate(change => {
     console.log(change.before.data());
     console.log(change.after.data())
-    let batch = db.batch();
+    if (change.before.data().imageUrl !== change.after.data().imageUrl) {
+      console.log("Image has changed");
+      let batch = db.batch();
+      return db.collection("screams").where("userHandle", "==", change.before.data().handle).get()
+        .then(data => {
+          data.forEach(doc => {
+            const scream = db.doc(`/screams/${doc.id}`);
+            batch.update(scream, { userImage: change.after.data().imageUrl });
+          })
 
-    return db.collection("screams").where("userHandle", "==", change.before.data().handle).get()
+          return batch.commit();
+        })
+    } else return true;
+  });
+
+exports.onScreamDelete = functions.firestore.document(`/screams/{screamId}`)
+  .onDelete((snapshot, context) => {
+    const screamId = context.params.screamId;
+    const batch = db.batch();
+    return db.collection("comments").where("screamId", "==", screamId).get()
       .then(data => {
         data.forEach(doc => {
-          const scream = db.doc(`/screams/${doc.id}`)
+          batch.delete(db.doc(`/comments/${doc.id}`));
         })
+        return db.collection(`likes`).where("screamId", "==", screamId).get()
+      })
+      .then(data => {
+        data.forEach(doc => {
+          batch.delete(db.doc(`/likes/${doc.id}`));
+        })
+        return db.collection(`notifications`).where("screamId", "==", screamId).get()
+      })
+      .then(data => {
+        data.forEach(doc => {
+          batch.delete(db.doc(`/notifications/${doc.id}`));
+        })
+        return batch.commit();
+      })
+      .catch(err => {
+        console.error(err);
       })
   })
 
