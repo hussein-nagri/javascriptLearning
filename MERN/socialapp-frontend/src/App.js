1a090fc107ad95cfc9cb35fbcf540b21d6bc321d
Hussein <hznagri@uwaterloo.ca>
2019-12-14 21:23:30 -0500

    added proxy and backend request using axios

--- MERN/socialapp-frontend/src/App.js
+++ MERN/socialapp-frontend/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.css';
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
+import createTheme from '@material-ui/core/styles/createMuiTheme';
 
 
 //Components
@@ -10,22 +12,45 @@ import Navbar from './components/Navbar';
 import home from './pages/home';
 import login from './pages/login';
 import signup from './pages/signup';
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#33c9dc',
+      main: '#00bcd4',
+      dark: '008394',
+      contrastText: '#fff'
+    },
+    secondary: {
+      light: '#ff6333',
+      main: '#ff3d00',
+      dark: '#b22a00',
+      contrastText: '#fff'
+    }
+  },
+  typography: {
+    useNextVariants: true
+  }
+});
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Navbar></Navbar>
-        <div className="container">
-          <Switch>
-            <Route exact path="/" component={home}></Route>
-            <Route exact path="/login" component={login}></Route>
-            <Route exact path="/signup" component={signup}></Route>
-          </Switch>
-        </div>
-      </Router>
-    </div>
+    <MuiThemeProvider theme={theme}>
+      <div className="App">
+        <Router>
+          <Navbar></Navbar>
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={home}></Route>
+              <Route exact path="/login" component={login}></Route>
+              <Route exact path="/signup" component={signup}></Route>
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    </MuiThemeProvider>
   );
 }
 
--- MERN/socialapp-frontend/src/pages/home.js
+++ MERN/socialapp-frontend/src/pages/home.js
@@ -1,11 +1,40 @@
 import React, { Component } from 'react'
+import { Grid } from '@material-ui/core/';
+import axios from 'axios'
+
+import Scream from '../components/Scream'
 
 class home extends Component {
+  state = {
+    screams: null
+  }
+  componentDidMount() {
+    axios.get("/screams")
+      .then(res => {
+        console.log(res.data)
+        this.setState({
+          screams: res.data
+        });
+      })
+      .catch(err => {
+        console.error(err);
+      })
+  }
   render() {
+    let recemtScreamsmarkup = this.state.screams ? (
+      this.state.screams.map(scream => <Scream key={scream.screamId} scream={scream} />)
+    ) : (
+        <p> Loading...</p>
+      )
     return (
-      <div>
-        <h1>home page</h1>
-      </div>
+      <Grid container spacing={16} >
+        <Grid item sm={8} xs={12}>
+          {recemtScreamsmarkup}
+        </Grid>
+        <Grid item sm={4} xs={12}>
+          <p>Profile ...</p>
+        </Grid>
+      </Grid >
     )
   }
 }
