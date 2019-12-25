9cb72bd0cc2c528ab1ba4ec852a2d0f9a4c0b242
Hussein <hznagri@uwaterloo.ca>
2019-12-24 23:30:57 -0500

    added dependancy

---
+++ MERN/jobboard/jobboard-worker/tasks/fetch-github.js
@@ -0,0 +1,27 @@
+var fetch = require("node-fetch");
+
+const baseUrl = 'https://jobs.github.com/positions.json'
+
+async function fetchGithub() {
+
+
+
+  let resultCount = 1, onPage = 0;
+  const allJobs = [];
+
+
+  while (resultCount > 0) {
+    const res = await fetch(`${baseUrl}?page=${onPage}`)
+    const jobs = await res.json();
+    allJobs.push(jobs)
+    resultCount = jobs.length
+    console.log('got ', resultCount);
+    onPage++;
+  }
+
+  console.log(`got `, allJobs.length, ` Jobs`)
+}
+
+fetchGithub();
+
+module.exports = fetchGithub;
