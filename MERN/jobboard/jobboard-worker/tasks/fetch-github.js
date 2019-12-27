65e1513df30b5bb0b8ea09d68eec5314512f7038
Hussein <hznagri@uwaterloo.ca>
2019-12-26 20:42:23 -0500

    filter algorithm

--- MERN/jobboard/jobboard-worker/tasks/fetch-github.js
+++ MERN/jobboard/jobboard-worker/tasks/fetch-github.js
@@ -9,7 +9,7 @@ async function fetchGithub() {
   let resultCount = 1, onPage = 0;
   const allJobs = [];
 
-
+  //fetch all pages
   while (resultCount > 0) {
     const res = await fetch(`${baseUrl}?page=${onPage}`)
     const jobs = await res.json();
@@ -18,10 +18,29 @@ async function fetchGithub() {
     console.log('got ', resultCount);
     onPage++;
   }
+  // filter algo
+  const jrJobs = allJobs.filter(job => {
+    const jobTitle = job.title.toLowerCase();
+    // algo logic
+    if (
+      jobTitle.includes('senior') ||
+      jobTitle.includes('manager') ||
+      jobTitle.includes('sr.') ||
+      jobTitle.includes('architect')
+    ) {
+      return false
+    }
+    return true;
+  })
+
+  console.log('filtered down to', jrJobs.length);
+
 
   console.log(`got `, allJobs.length, ` Jobs`)
+
+
 }
 
 fetchGithub();
 
-module.exports = fetchGithub;
+module.exports = fetchGithub; 
