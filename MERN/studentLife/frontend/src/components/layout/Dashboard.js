4fc8ab4a66e66bb7a29dd641f95d8fbc29d97a60
Hussein <hznagri@uwaterloo.ca>
2020-01-28 08:33:08 -0500

    added card for login page

--- MERN/studentLife/frontend/src/components/layout/Dashboard.js
+++ MERN/studentLife/frontend/src/components/layout/Dashboard.js
@@ -1,11 +1,65 @@
 import React, { Component } from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
 
 class Dashboard extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+    }
+    this.useStyles = this.useStyles.bind(this);
+
+  }
+
+
+  useStyles = () => makeStyles({
+    card: {
+      maxWidth: 345,
+    },
+    media: {
+      height: 140,
+    },
+  });
+
   render() {
+
+    const classes = this.useStyles();
+
     return (
-      <div>
-        hello world
-      </div>
+      <Card className={classes.card}>
+        <CardActionArea>
+          <CardMedia
+            className={classes.media}
+            image="/static/images/cards/contemplative-reptile.jpg"
+            title="Contemplative Reptile"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              Welcome! Are you attending a hackathon?
+          </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+              across all continents except Antarctica
+          </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            Share
+        </Button>
+          <Button size="small" color="primary">
+            Learn More
+        </Button>
+        </CardActions>
+      </Card>
     )
   }
 }
