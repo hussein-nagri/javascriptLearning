20cab05ec0e22a7e87ec3682cddcaf38d5156a3a
Hussein <hznagri@uwaterloo.ca>
2020-02-04 22:52:22 -0500

    added mounting call to backend for the upcomgin hackathon data

--- MERN/studentLife/frontend/src/components/layout/Hackathons.js
+++ MERN/studentLife/frontend/src/components/layout/Hackathons.js
@@ -1,6 +1,20 @@
 import React, { Component, useEffect } from 'react'
+import axios from 'axios';
 
-export class Hackathons extends Component {
+
+class Hackathons extends Component {
+
+  componentDidMount(prevProps) {
+
+
+    axios.get("/api/hackathons")
+      .then(res => {
+        console.log(res);
+      })
+  }
+
+
+
   render() {
     return (
       <div>
--- MERN/studentLife/routes/api/hackathons.js
+++ MERN/studentLife/routes/api/hackathons.js
@@ -17,7 +17,11 @@ router.get(
   async (req, res) => {
 
     console.log(req.body)
-
+    return res.status(200).json({ msg: "Succes" });
 
   }
 );
+
+
+
+module.exports = router;
