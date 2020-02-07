bcf3ad4cfee892b6912b079537881297b8b83b23
Hussein <hznagri@uwaterloo.ca>
2020-02-07 18:56:49 -0500

    initial ui setup for hackathon page

--- MERN/studentLife/frontend/src/components/layout/Hackathons.js
+++ MERN/studentLife/frontend/src/components/layout/Hackathons.js
@@ -1,4 +1,4 @@
-import React, { Component, useEffect } from 'react'
+import React, { Component, useEffect, Fragment } from 'react'
 import axios from 'axios';
 
 
@@ -34,17 +34,55 @@ class Hackathons extends Component {
       var arr = this.state.data.split("[")[1].slice(0, -2);
       arr = arr.split("}, {");
       arr[0] = arr[0].substr(1);
+      arr = arr.map(dict => dict.split(","));
     }
     console.log(arr);
     return (
-      //     {
-      //       if(this.state.data) {
-      //     <div>{data}</div>
-      //   }
-      // }
-      < div >
-        WElcome to the hackathon page
-      </div >
+      < Fragment >
+        {
+          arr && arr.map((dict, index) =>
+            <Fragment key={index}>
+
+              <div className="container">
+                <div className="card-deck mb-3 text-center">
+                  <div className="card-body shadow-sm mt-3 mr-3" style={{ width: "12rem" }}>
+                    <a href={dict["link"]}>
+                      <img src={dict["logo"]} className="card-img-top" />
+                    </a>
+                    <hr />
+                    <ul className="list-unstyled">
+                      <h4 className="card-title pricing-card-title">{dict['name']}</h4>
+                      <li>{dict['start date']}</li>
+                      <li>{dict['city']}</li>
+                      <li>{dict['state']}</li>
+                      <li><a href={dict['link']} target="_blank">Website Link</a></li>
+                    </ul>
+                  </div>
+
+                </div>
+              </div>
+              {/* 
+              <div>
+                {dict[0]}
+              </div>
+              <div>
+                {dict[1]}
+              </div>
+              <div>
+                {dict[2]}
+              </div>
+              <div>
+                Hello
+            </div> */}
+            </Fragment>
+
+
+          )
+
+        }
+
+      </Fragment>
+
     )
   }
 }
