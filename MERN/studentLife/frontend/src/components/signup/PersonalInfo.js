233cb9ce95efcc166f3b7bf396d0081344d7aa17
Hussein <hznagri@uwaterloo.ca>
2020-03-05 14:45:42 -0500

    Added more items to signup component

--- MERN/studentLife/frontend/src/components/signup/PersonalInfo.js
+++ MERN/studentLife/frontend/src/components/signup/PersonalInfo.js
@@ -15,10 +15,13 @@ import Radio from '@material-ui/core/Radio';
 import RadioGroup from '@material-ui/core/RadioGroup';
 import FormHelperText from '@material-ui/core/FormHelperText';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
-import FormControl from '@material-ui/core/FormControl';
 import FormLabel from '@material-ui/core/FormLabel';
 import { makeStyles } from '@material-ui/core/styles';
-
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import Button from '@material-ui/core/Button';
 
 
 const useStyles = makeStyles(theme => ({
@@ -40,11 +43,15 @@ class PersonalInfo extends Component {
       gender: "female",
       address: "",
       city: "",
+      province: "",
       date: "",
+      open: false,
       error: true
     }
     this.onChangeHandler = this.onChangeHandler.bind(this);
     this.onChangeDate = this.onChangeDate.bind(this);
+    this.onOpen = this.onOpen.bind(this);
+
   }
 
   onChangeHandler = (e) => {
@@ -53,6 +60,9 @@ class PersonalInfo extends Component {
 
   onChangeDate = date => this.setState({ date })
 
+  onOpen = (e) => {
+    this.setState({ open: !(this.state.open) });
+  }
 
 
   render() {
@@ -66,7 +76,7 @@ class PersonalInfo extends Component {
           <div className="col textfield">
             <TextField
               autoFocus
-              
+
               required
               className="textfield"
               name="firstName"
@@ -136,6 +146,46 @@ class PersonalInfo extends Component {
             style={{ marginTop: "100px", marginLeft: "100px" }}
 
           />
+          <TextField
+            autoFocus
+            required
+            className="textfield"
+            name="city"
+            placeholder="City"
+            value={this.state.city}
+            onChange={e => this.onChangeHandler(e)}
+            variant="outlined"
+            style={{ marginTop: "100px", marginLeft: "100px" }}
+
+          />
+
+
+          <div className="col col-7">
+
+
+
+            <FormControl style={{marginTop:"5%"}} className="col-3">
+              <InputLabel id="demo-controlled-open-select-label">Province</InputLabel>
+
+              <Select
+                labelId="demo-controlled-open-select-label"
+                id="demo-controlled-open-select"
+                open={this.state.open}
+                onClose={this.onOpen}
+                onOpen={this.onOpen}
+                value={this.state.province}
+                onChange={this.onChangeHandler}
+              >
+                <MenuItem value="">
+                  <em>None</em>
+                </MenuItem>
+                <MenuItem value={10}>Ontario</MenuItem>
+                <MenuItem value={20}>Manitoba</MenuItem>
+                <MenuItem value={30}>Quebec</MenuItem>
+              </Select>
+            </FormControl>
+
+          </div>
 
         </div>
       </form >
