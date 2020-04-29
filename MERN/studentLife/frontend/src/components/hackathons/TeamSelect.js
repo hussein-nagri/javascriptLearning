4ba1ebff7d1ec228af888176f8b8f26992a626bf
Hussein <hznagri@uwaterloo.ca>
2020-04-29 12:25:25 -0400

    basic webpage telling customers to check their email for the team

--- MERN/studentLife/frontend/src/components/hackathons/TeamSelect.js
+++ MERN/studentLife/frontend/src/components/hackathons/TeamSelect.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react'
 
+
+import DoneIcon from '@material-ui/icons/Done';
+
+
+
+
+
+
 class TeamSelect extends Component {
   render() {
     return (
       <div>
-        yoo
-        
+        <div style={{ textAlign: "center" }}>
+          <DoneIcon
+            style={{
+              color: "green",
+              height: "40%",
+              width: "40%",
+            }}
+          />
+        </div >
+        <div style={{ textAlign: "center", fontSize: "x-large" }}>
+        Please check your email. We will update it once we have an optimal team for you
+  
+        </div>
+
+
       </div>
     )
   }
