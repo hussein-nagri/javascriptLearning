50a6f91653c81e282711b2d823f8602c0fcea813
Hussein <hznagri@uwaterloo.ca>
2020-06-01 23:21:30 -0400

    issue is w asynchronousy

--- MERN/studentLife/helpers/cron.js
+++ MERN/studentLife/helpers/cron.js
@@ -17,41 +17,45 @@ var job = new CronJob('* * * * *', async function () {
     port: 80,
   });
 
-  const items = promise.data;
-  var result_of_hackathon_names = items.map(dict => {
-    return dict.name;
-  })
-  console.log(result_of_hackathon_names);
-  console.log("he");
-  for (let i = 0; i < result_of_hackathon_names.length; ++i) {
-    let ht_name = result_of_hackathon_names[i];
-    console.log(ht_name);
+  // const items = promise.data;
+  // var result_of_hackathon_names = items.map(dict => {
+  //   return dict.name;
+  // })
+  // console.log(result_of_hackathon_names);
+  // console.log("he");
+  // for (let i = 0; i < result_of_hackathon_names.length; ++i) {
+  //   let ht_name = result_of_hackathon_names[i];
+  //   console.log(ht_name);
 
-    ans = await Teams.collection.find({ 'hackathon': ht_name })
-    console.log(ans);
-  }
-
-
-  console.log("OT in")
+  //   ans = await Teams.collection.find({ 'hackathon': ht_name })
+  //   console.log(ans);
+  // }
 
 
-
-  const msg = {
-    to: 'husseinnagri@hotmail.com', //'sabihah.k2001@gmail.com',
-    from: 'hznagri@uwaterloo.ca',
-    subject: 'This is to see if my api works to losers',
-    text: 'It works bc the hoe got it',
-    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-  };
-  try {
-    await sgMail.send(msg);
-    console.log("sent");
-  } catch (error) {
-    console.log(error);
-  }
+  // console.log("OT in")
 
 
-  console.log("Finished");
+
+  // const msg = {
+  //   to: 'husseinnagri@hotmail.com', //'sabihah.k2001@gmail.com',
+  //   from: 'hznagri@uwaterloo.ca',
+  //   subject: 'This is to see if my api works to losers',
+  //   text: 'It works bc the hoe got it',
+  //   html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+  // };
+  // try {
+  //   await sgMail.send(msg);
+  //   console.log("sent");
+  // } catch (error) {
+  //   console.log(error);
+  // }
+
+
+  // console.log("Finished");
+
+  let hello = await axios.get('https://jsonplaceholder.typicode.com/todos/1');
+  let data = await hello.json();
+  console.log(data);
 }, null, true, 'America/Los_Angeles');
 
 
